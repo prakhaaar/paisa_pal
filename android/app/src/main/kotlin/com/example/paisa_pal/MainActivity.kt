4e54a2cd044b89a71f3ff1dbadd923c4a9937a95
Prakhar <mprakhar07@gmail.com>
2025-09-24 22:50:59 +0530

    Initial commit of Paisa Pal Flutter project

---
+++ android/app/src/main/kotlin/com/example/paisa_pal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.paisa_pal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:paisa_pal/project/real_main.dart';
+
+void main() async {
+  realMain();
+}
